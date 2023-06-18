1675d3561bc7e4a652033d741661360b24a9250f
Eddie Obrams <eddieobrams@gmail.com>
2023-06-18 20:27:08 +0100

    Updated API - Put, Post, Get, Delete added - it is now a fully funcitonal CRUD API.

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const config = require('./config');
 const express = require('express');
+const mongoose = require('mongoose');
+const products = require("./routes/products");
 const app = express();
 
-// Routes
+app.use("/products", products);
+app.use(express.json());
+app.use(express.urlencoded())
+    // Routes
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "Hello" })
+    res.status(200).json({ message: "Hello, this is my API" });
 })
 
-// Run API server
-app.listen(config.PORT, () => {
-    console.log(`API running on port ${config.PORT}`);
-})
+// Connect to MongoDB using Mongoose
+mongoose.connect(config.MONGOOSEURI)
+    .then(() => {
+        console.log("Successfully connected to MongoDB🍃");
+        // Run API server
+        app.listen(config.PORT, () => {
+            console.log(`API running on port ${config.PORT}`);
+        })
+    }).catch((error) => {
+        console.log(`Something bad happened: ${error}`);
+    })
